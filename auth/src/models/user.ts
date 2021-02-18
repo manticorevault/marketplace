57babfae0380739bc96b20229063bd9e402b0298
manticorevault <artur_seilah@hotmail.com>
2021-02-18 13:52:09 +0000

    Created interfaces to better connect Mongoose with TypeScript

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,16 +1,29 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose'
 
-const userSchema = new mongoose Schema({
-    email: {
-        type: String,
-        required: true 
-    },
-    password: {
-        type: String,
-        required: true 
-    }
+interface UserRequirements {
+  email: string
+  password: string
+}
+
+interface UserModel extends mongoose.Model<any> {
+  build(requirements: UserRequirements): any
+}
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
 })
 
-const User = mongoose.model("User", userSchema)
+userSchema.statics.build = (requirements: UserRequirements) => {
+  return new User(requirements)
+}
 
-export { User };
+const User = mongoose.model<any, UserModel>('User', userSchema)
+
+export { User }
