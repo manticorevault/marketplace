8c1be56673681ce2706a6e65bcb5a9fbb095a4c3
manticorevault <artur_seilah@hotmail.com>
2021-03-01 18:48:42 +0000

    Updated messages

--- auth/src/errors/database-connection-error.ts
+++ auth/src/errors/database-connection-error.ts
@@ -3,12 +3,12 @@ import { CustomError } from './custom-error'
 export class DatabaseConnectionError extends CustomError {
   statusCode = 500
   constructor() {
-    super('There was an error trying to connect to the database.')
+    super('There was an error trying to connect to the database!')
 
     Object.setPrototypeOf(this, DatabaseConnectionError.prototype)
   }
 
   serializeErrors() {
-    return [{ message: 'Database connection error' }]
+    return [{ message: 'Database connection error!' }]
   }
 }
